4c2cffc855e2edc01c49fae703828ed77971cdc0
Raphael Allotey Baddoo <rabaddoo@st.ug.edu.gh>
2022-08-26 23:56:13 -0700

    Login page done, working on the manage users page, hmm.

---
+++ Inventory/ManageUsers.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Inventory
+{
+    partial class ManageUsers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.unameTb = new System.Windows.Forms.TextBox();
+            this.FnameTb = new System.Windows.Forms.TextBox();
+            this.passwordTb = new System.Windows.Forms.TextBox();
+            this.phoneTb = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.UsersGV = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.UsersGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Crimson;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(750, 87);
+            this.panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(121, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(500, 40);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "INVENTORY MANAGEMENT SYSTEM";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(275, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Manage Users";
+            // 
+            // unameTb
+            // 
+            this.unameTb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.unameTb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.unameTb.Location = new System.Drawing.Point(12, 149);
+            this.unameTb.Name = "unameTb";
+            this.unameTb.Size = new System.Drawing.Size(183, 23);
+            this.unameTb.TabIndex = 1;
+            this.unameTb.Text = "Username";
+            // 
+            // FnameTb
+            // 
+            this.FnameTb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FnameTb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.FnameTb.Location = new System.Drawing.Point(12, 188);
+            this.FnameTb.Name = "FnameTb";
+            this.FnameTb.Size = new System.Drawing.Size(183, 23);
+            this.FnameTb.TabIndex = 2;
+            this.FnameTb.Text = "Fullname";
+            // 
+            // passwordTb
+            // 
+            this.passwordTb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.passwordTb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.passwordTb.Location = new System.Drawing.Point(12, 226);
+            this.passwordTb.Name = "passwordTb";
+            this.passwordTb.Size = new System.Drawing.Size(183, 23);
+            this.passwordTb.TabIndex = 3;
+            this.passwordTb.Text = "Password";
+            // 
+            // phoneTb
+            // 
+            this.phoneTb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.phoneTb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.phoneTb.Location = new System.Drawing.Point(12, 261);
+            this.phoneTb.Name = "phoneTb";
+            this.phoneTb.Size = new System.Drawing.Size(183, 23);
+            this.phoneTb.TabIndex = 4;
+            this.phoneTb.Text = "Phone number";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Crimson;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(12, 307);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 35);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Crimson;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(77, 307);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 35);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Edit";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Crimson;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(150, 307);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(59, 35);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Crimson;
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(77, 358);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(67, 35);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Home";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Crimson;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 503);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(750, 18);
+            this.panel2.TabIndex = 9;
+            // 
+            // UsersGV
+            // 
+            this.UsersGV.BackgroundColor = System.Drawing.Color.White;
+            this.UsersGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.UsersGV.Location = new System.Drawing.Point(281, 121);
+            this.UsersGV.Name = "UsersGV";
+            this.UsersGV.Size = new System.Drawing.Size(429, 355);
+            this.UsersGV.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Corbel", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(722, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "X";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // ManageUsers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(750, 521);
+            this.Controls.Add(this.UsersGV);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.phoneTb);
+            this.Controls.Add(this.passwordTb);
+            this.Controls.Add(this.FnameTb);
+            this.Controls.Add(this.unameTb);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ManageUsers";
+            this.Text = "Form2";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.UsersGV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox unameTb;
+        private System.Windows.Forms.TextBox FnameTb;
+        private System.Windows.Forms.TextBox passwordTb;
+        private System.Windows.Forms.TextBox phoneTb;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView UsersGV;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Inventory/ManageUsers.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Inventory
+{
+    public partial class ManageUsers : Form
+    {
+        public ManageUsers()
+        {
+            InitializeComponent();
+        }
+        SqlConnection Con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\Raphael Baddoo\Documents\Database.mdf"";Integrated Security=True;Connect Timeout=30");
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        void populate()
+        {
+            try
+            {
+                Con.Open();
+                string Myquery = "select"  from UserTb1; 
+                Con.Close();
+            }
+            catch
+            {
+
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            try 
+            {
+                Con.Open();
+                SqlCommand cmd = new SqlCommand("insert into UserTb1 values('" + unameTb.Text + "','" + FnameTb.Text + "','" + passwordTb.Text + "','" + phoneTb.Text + "')", Con);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("User Successfully Added");
+                Con.Close();
+            }
+            catch
+            {
+
+            }
+
+        }
+    }
+}
